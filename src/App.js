0ddc9c1a34b5e3aa682d9c4020d688b53e01286e
BlagoKolev <blagovest.kolev@gmail.com>
2021-11-22 00:06:53 +0200

    Establish a connection with Firebase for back-end

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@ import './App.css';
 import Header from './components/Header/Header.js'
 import Footer from './components/Footer/Footer.js'
 import Main from './components/Main/Main.js'
+
+import { db } from './firebase-config.js'
+import { collection, getDocs } from 'firebase/firestore';
+import { useEffect } from 'react';
+import reportWebVitals from './reportWebVitals';
+import { Route } from 'react-router';
+
 function App() {
+
+  // let usersRef = collection(db, 'users');
+
+  // useEffect(() => {
+  //   const getUsers = async () => {
+  //     let data = await getDocs(usersRef);
+  //     let response = data.docs.map( x=> ({...x.data(),id: x.id}));
+  //     console.log(response)
+  //   };
+  //   getUsers();
+  // }, [])
+
   return (
     <div className="App">
       <Header />
---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from '@firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyApQhIVdgRumWc6zULMsRTZ15OvCJreCd4",
+  authDomain: "bookshelf-3c638.firebaseapp.com",
+  projectId: "bookshelf-3c638",
+  storageBucket: "bookshelf-3c638.appspot.com",
+  messagingSenderId: "348154374651",
+  appId: "1:348154374651:web:f1a2837894bb9eae68835e"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
