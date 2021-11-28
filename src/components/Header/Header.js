b8b13812c649d52ca7911b3039d748be018cf70b
BlagoKolev <blagovest.kolev@gmail.com>
2021-11-29 01:29:41 +0200

    Setted up Nav-bar to work properly
     I Setted up Nav-bar to show properly content according to that is user is loged-in or is a guest.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,14 @@ import style from './Header.module.css';
 import { Route, Link } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
 import { auth } from '../../firebase-config';
-import {toast, ToastContainer} from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function Header({ username, isAuthenticated }) {
 
 
     const signOutNotify = () => {
-        toast.success("Successfully Signed-out !",  {position: toast.POSITION.TOP_CENTER});
+        toast.success("Successfully Signed-out !", { position: toast.POSITION.TOP_CENTER });
     };
 
     const onSignOut = async (e) => {
@@ -22,24 +22,39 @@ function Header({ username, isAuthenticated }) {
             <Link to="/" className={style.headerLogo}>BookShelf</Link>
             <nav className={style.headerNav}>
                 <ul className={style.headerNavUl}>
+
                     {isAuthenticated &&
                         <li className={style.headerNavElement}>
                             Hello: {username}
                         </li>}
-                    <li className={style.headerNavElement}>
-                        <a >Upload book</a>
-                    </li>
-                    <li className={style.headerNavElement}>
-                        <Link to="/login" className={style.headerNavLink}>Login</Link>
-                    </li>
-                    <li className={style.headerNavElement}>
-                        <Link to="/register" className={style.headerNavLink}>Register</Link>
-                    </li>
-                    <li className={style.headerNavElement}>
-                        <Link to="/"
-                            className={style.headerNavLink}
-                            onClick={onSignOut}>Logout</Link>
-                    </li>
+
+                    {isAuthenticated &&
+                        <li className={style.headerNavElement}>
+                            <a >Upload Book</a>
+                        </li>}
+
+                    {!isAuthenticated &&
+                        <li className={style.headerNavElement}>
+                            <Link to="/login" className={style.headerNavLink}>Login</Link>
+                        </li>}
+
+                    {!isAuthenticated &&
+                        <li className={style.headerNavElement}>
+                            <Link to="/register" className={style.headerNavLink}>Register</Link>
+                        </li>}
+
+                    {isAuthenticated &&
+                        <li className={style.headerNavElement}>
+                            <Link to="/register" className={style.headerNavLink}>My Books</Link>
+                        </li>}
+
+                    {isAuthenticated &&
+                        <li className={style.headerNavElement}>
+                            <Link to="/"
+                                className={style.headerNavLink}
+                                onClick={onSignOut}>Logout</Link>
+                        </li>}
+
                 </ul>
             </nav>
         </header>
