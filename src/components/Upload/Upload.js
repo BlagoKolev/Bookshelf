6924101e552ca65e01318ac93b86b42063c03518
BlagoKolev <blagovest.kolev@gmail.com>
2021-12-03 16:17:08 +0200

    Customize Form in Upload Component
    Add fields for: Author, Title, Review, Book cover URL, and make respectively
    to them States that holds fields values.

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -2,22 +2,28 @@ import { storage } from '../../firebase-config.js';
 import { getDownloadURL, ref, uploadBytesResumable } from '@firebase/storage';
 import { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
+import style from './Upload.module.css';
 
 function Upload() {
 
     const [progress, setProgress] = useState(0);
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [review, setReview] = useState("");
+    const [imgUrl, setImgUrl] = useState("");
+    const [file, setFile] = useState({});
 
     const onSubmitUpload = (e) => {
         e.preventDefault();
-        let file = e.target[0].files[0];
-        uploadFiles(file);
+        let fileToUpload = file;
+        uploadFiles(fileToUpload);
     };
 
-    const uploadFiles = (file) => {
+    const uploadFiles = (fileToUpload) => {
 
-        if (!file) { return };
-        const storageRef = ref(storage, `/books/${file.name}`);
-        const uploadTask = uploadBytesResumable(storageRef, file);
+        if (!fileToUpload) { return };
+        const storageRef = ref(storage, `/books/${fileToUpload.name}`);
+        const uploadTask = uploadBytesResumable(storageRef, fileToUpload);
 
         uploadTask.on("state_changed", (snapshot) => {
             const prog = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
@@ -35,21 +41,37 @@ function Upload() {
 
     toast.configure();
 
-    const onSuccessUploadNotify = () => toast.success("File uploaded successfully !",{
+    const onSuccessUploadNotify = () => toast.success("File uploaded successfully !", {
         position: toast.POSITION.TOP_CENTER
-      });
+    });
 
-      const onErrorUploadNotify = () => toast.error("Upload fail !",{
+    const onErrorUploadNotify = () => toast.error("Upload fail !", {
         position: toast.POSITION.TOP_CENTER
-      });
+    });
 
+    const getFile = (e) => {
+        setFile(e.target.files[0])
+    }
+   
 
     return (
-        <div>
-            <form onSubmit={onSubmitUpload}>
-                
-                <input type="file" />
-                <input type="submit" value="Upload" />
+        <div className={style.uploadContainer}>
+            <h1>Upload your new book here</h1>
+            <form onSubmit={onSubmitUpload} className={style.form}>
+                <label className={style.label}>Title
+                    <input type="text" className={style.input} onChange={(e) => { setTitle(e.target.value) }} />
+                </label>
+                <label className={style.label}>Author
+                    <input type="text" className={style.input} onChange={(e) => { setAuthor(e.target.value) }} />
+                </label>
+                <label className={style.label}>Review
+                    <textarea type="textarea" rows="10" className={style.input} onChange={(e) => { setReview(e.target.value) }} />
+                </label>
+                <label className={style.label}>Book cover URL
+                    <input type="text" className={style.input} onChange={(e) => { setImgUrl(e.target.value) }} />
+                </label>
+                <input type="file" className={style.fileField} onChange={getFile} />
+                <input type="submit" value="Upload" className={style.uploadBtn} />
             </form>
             <h3>Uploaded: {progress} %</h3>
         </div>
