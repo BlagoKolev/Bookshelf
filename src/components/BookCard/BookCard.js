2bd770f586247f00ad52f9d9e7bb4339ca54fe37
BlagoKolev <blagovest.kolev@gmail.com>
2021-12-15 10:56:27 +0200

    1. Changes to BookCard. 2.Changes to BookDetails.
    1. If book title is too long (over 25 symbols), then browser shows only 25 symbols of titile + "..."
    
    2. Made BookDetails with responsive design for Phones.

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -1,16 +1,18 @@
 import style from './BookCard.module.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function BookCard(props) {
-   
+
     return (
         <div className={style.container}>
             <div className={style.imageContainer}>
                 <img className={style.image} src={props.details.bookCoverUrl}></img>
                 {/* <img className={style.image} src="https://cdn.picpng.com/book/book-view-30965. */}
             </div>
-            <p>Title: {props.details.title}</p>
-          <Link to={`/bookDetails/${props.details.id}`} className={style.detailsBtn} details={props.details} >Details</Link>
+            {props.details.title.length > 25
+                ? <div>Title: {props.details.title.slice(0, 25)} ...</div>
+                : <p>Title: {props.details.title}</p>}
+            <Link to={`/bookDetails/${props.details.id}`} className={style.detailsBtn} details={props.details} >Details</Link>
         </div>
     )
 }
